11b448d6ad640486e852b3afaccf054564d9cbe0
Daniel Mahal <danielmahal@gmail.com>
2019-02-25 18:14:43 +0100

    Simpler particles so that it works on phones + tablets

--- main.js
+++ main.js
@@ -1,26 +1,5 @@
-const params = {
-  debugField: false
-}
-
-window.addEventListener('keydown', e => {
-  console.log(e.keyCode)
-
-  if (e.keyCode === 68) { // key: d
-    params.debugField = !params.debugField
-  }
-})
-
-Promise.all([
-  window.fetch('./lib/cellular2D.glsl').then(res => res.text()),
-  loadImage('./assets/life-forms.svg')
-]).then(([
-  cellular2D,
-  typeImage
-]) => {
-  const regl = window.createREGL({
-    extensions: ['OES_texture_float'],
-    pixelRatio: 1
-  })
+loadImage('./assets/life-forms.svg').then(typeImage => {
+  const regl = window.createREGL({ pixelRatio: 1 })
 
   const canvas = document.querySelector('canvas')
 
@@ -43,7 +22,7 @@ Promise.all([
     typeHeight = typeImage.height * (typeWidth / typeImage.width)
   } else {
     typeHeight = typeCanvas.height
-    typeWidth = typeCanvas.width * (typeHeight / typeImage.height)
+    typeWidth = typeImage.width * (typeHeight / typeImage.height)
   }
 
   typeWidth *= typeSize
@@ -75,70 +54,77 @@ Promise.all([
     flipY: true
   })
 
-  const noiseFramebuffer = regl.framebuffer({
-    depthStencil: false,
-    color: regl.texture({
-      type: 'float',
-      width: 256,
-      height: 256,
-      wrapS: 'mirror',
-      wrapT: 'mirror'
-    })
-  })
-
-  const fieldFramebuffer = regl.framebuffer({
-    depthStencil: false,
-    color: regl.texture({
-      type: 'float',
-      width: width,
-      height: height
-    })
-  })
-
-  const count = (width * height) / 30
+  const count = (width * height) / 80
   const squared = Math.ceil(Math.sqrt(count))
   const [cols, rows] = [squared, squared]
 
   const buffer = regl.buffer(
     times(cols, col =>
-      times(rows, row =>
-        [col, row]
-      )
+      times(rows, row => [
+        (Math.random() - 0.5) / width,
+        (Math.random() - 0.5) / height,
+        Math.random() * Math.PI * 2,
+        Math.random() * Math.PI * 2
+      ])
     )
   )
 
-  const createPingPong = data => times(2, () =>
-    regl.framebuffer({
-      depthStencil: false,
-      color: regl.texture({
-        type: 'float',
-        data
-      })
-    })
-  )
-
-  const positions = createPingPong(
-    times(cols, () =>
-      times(rows, () =>
-        [Math.random(), Math.random(), 0, 0]
-      )
-    )
-  )
-
-  const velocities = createPingPong(
-    times(cols, () =>
-      times(rows, () =>
-        [0, 0, 0, 0]
-      )
-    )
-  )
+  const stride = 4 * (2 + 2)
 
   const current = states => ({ tick }) => states[tick % 2]
   const previous = states => ({ tick }) => states[(tick + 1) % 2]
 
-  const quad = {
+  const draw = regl({
     vert: `
-      precision mediump float;
+      precision highp float;
+      attribute vec2 freq;
+      attribute vec2 phase;
+      uniform float time;
+
+      void main () {
+        vec2 position = freq.xy * time + phase.xy;
+
+        position = position - 1.0 * floor(position / 1.0);
+
+        gl_PointSize = 1.0;
+        gl_Position = vec4(position * 2.0 - 1.0, 0.0, 1.0);
+      }
+    `,
+
+    frag: `
+      precision highp float;
+
+      void main () {
+        gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+      }
+    `,
+
+    attributes: {
+      freq: {
+        buffer,
+        stride,
+        offset: 0
+      },
+
+      phase: {
+        buffer,
+        stride,
+        offset: 8
+      }
+    },
+
+    uniforms: {
+      time: ({ time }) => time * 40
+    },
+
+    count: count,
+
+    primitive: 'points'
+  })
+
+  const output = regl({
+    vert: `
+      precision highp float;
       attribute vec2 position;
       varying vec2 uv;
 
@@ -160,176 +146,10 @@ Promise.all([
     elements: [
       [0, 1, 2],
       [2, 3, 0]
-    ]
-  }
-
-  const drawNoise = regl({
-    ...quad,
+    ],
 
     frag: `
-      precision mediump float;
-      varying vec2 uv;
-      uniform float aspect;
-      uniform float scale;
-      uniform float seed;
-      uniform vec2 direction;
-
-      ${cellular2D}
-
-      float rand (vec2 co){
-        return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453);
-      }
-
-      void main () {
-        vec2 noise;
-        vec2 sample = uv * scale * vec2(aspect, 1.0) + vec2(seed * 1000.0, 0);
-
-        noise += cellular2D(sample);
-        noise.x -= 0.4;
-        noise.y -= 0.7;
-        noise += direction;
-
-        // noise += (rand(uv) - 0.5) * 0.2;
-
-        gl_FragColor = vec4(noise, 0.0, 1.0);
-      }
-    `,
-
-    uniforms: {
-      aspect: width / height,
-      scale: regl.prop('scale'),
-      seed: regl.prop('seed'),
-      direction: regl.prop('direction')
-    }
-  })
-
-  const drawField = regl({
-    ...quad,
-
-    frag: `
-      precision mediump float;
-      varying vec2 uv;
-      uniform float time;
-      uniform sampler2D noise;
-
-      void main () {
-        vec2 color = vec2(
-          texture2D(noise, uv + sin(time / 80.0)).x,
-          texture2D(noise, uv + cos(time / 30.0)).y
-        );
-
-        gl_FragColor = vec4(color, 0.0, 1.0);
-      }
-    `,
-
-    uniforms: {
-      time: regl.context('time'),
-      noise: noiseFramebuffer,
-    }
-  })
-
-  const updateVelocities = regl({
-    ...quad,
-
-    frag: `
-      precision mediump float;
-      varying vec2 uv;
-      uniform sampler2D velocityField;
-      uniform sampler2D previousPositions;
-      uniform sampler2D previousVelocities;
-
-      void main () {
-        vec3 position = texture2D(previousPositions, uv).xyz;
-        vec3 velocity = texture2D(previousVelocities, uv).xyz;
-        vec3 field = texture2D(velocityField, position.xy).xyz;
-
-        velocity += field / 2000.0;
-        velocity *= 0.8;
-
-        gl_FragColor = vec4(velocity, 1.0);
-      }
-    `,
-
-    framebuffer: current(velocities),
-
-    uniforms: {
-      velocityField: regl.prop('velocityField'),
-      previousPositions: previous(positions),
-      previousVelocities: previous(velocities)
-    }
-  })
-
-  const updatePositions = regl({
-    ...quad,
-
-    frag: `
-      precision mediump float;
-      varying vec2 uv;
-      uniform sampler2D previousPositions;
-      uniform sampler2D currentVelocities;
-
-      void main () {
-        vec3 position = texture2D(previousPositions, uv).xyz;
-        vec3 velocity = texture2D(currentVelocities, uv).xyz;
-
-        position += velocity;
-        position.xy = position.xy - 1.0 * floor(position.xy / 1.0);
-
-        gl_FragColor = vec4(position, 1.0);
-      }
-    `,
-
-    framebuffer: current(positions),
-
-    uniforms: {
-      previousPositions: previous(positions),
-      currentVelocities: previous(velocities)
-    }
-  })
-
-  const draw = regl({
-    vert: `
-      precision mediump float;
-      attribute vec2 index;
-      uniform vec2 dimensions;
-      uniform sampler2D currentPositions;
-
-      void main () {
-        vec2 sample = index / dimensions;
-        vec3 position = texture2D(currentPositions, sample).xyz;
-
-        gl_PointSize = 1.0;
-        gl_Position = vec4(position * 2.0 - 1.0, 1.0);
-      }
-    `,
-
-    frag: `
-      void main () {
-        gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
-      }
-    `,
-
-    attributes: {
-      index: buffer
-    },
-
-    uniforms: {
-      dimensions: [cols, rows],
-      currentPositions: current(positions)
-    },
-
-    count: count,
-
-    primitive: 'points',
-
-    framebuffer: drawFramebuffer
-  })
-
-  const output = regl({
-    ...quad,
-
-    frag: `
-      precision mediump float;
+      precision highp float;
       varying vec2 uv;
       uniform sampler2D texture;
       uniform sampler2D typeTexture;
@@ -361,12 +181,7 @@ Promise.all([
           if (neighbors <= 3.0 + self && neighbors >= 3.0) {
             color += 1.0;
           }
-        } else {
-
         }
-
-        // color += texture2D(previous, uv).rgb;
-        // color -= 0.05;
 
         gl_FragColor = vec4(color, 1.0);
       }
@@ -380,34 +195,14 @@ Promise.all([
     }
   })
 
-  noiseFramebuffer.use(() => {
-    drawNoise({
-      scale: 3,
-      seed: Math.random(),
-      direction: [0.3, 0]
-    })
-  })
-
   const currentOutputBuffer = current(outputFramebuffers)
   const previousOutputBuffer = previous(outputFramebuffers)
 
   regl.frame(context => {
-    if (params.debugField) {
-      return drawField()
-    }
-
-    fieldFramebuffer.use(() => drawField())
-
-    updateVelocities({
-      velocityField: fieldFramebuffer
-    })
-
-    updatePositions()
-
     drawFramebuffer.use(() => {
       regl.clear({
         color: [0, 0, 0, 1],
-        depth: 0
+        depth: 1
       })
 
       draw()
